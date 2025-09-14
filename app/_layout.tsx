fdf3406fab697831012ddb09aff2da605c06dd8e
Waqar Khan <waqar.mrwt424@gmail.com>
2025-09-14 21:01:52 +0500

    Initial commit

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,13 +1,10 @@
 import { Stack } from "expo-router";
-import './global.css';
-
+import "./global.css";
 
 export default function RootLayout() {
   return (
     <Stack screenOptions={{ headerShown: false }}>
-      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+      <Stack.Screen name="index" options={{ headerShown: false }} />
     </Stack>
   );
 }
-
-
