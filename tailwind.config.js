fab46eeb6706a29eec066c7529b1d18132d66a73
Waqar Khan <waqar.mrwt424@gmail.com>
2025-09-29 18:59:47 +0500

    Initial commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  // NOTE: Update this to include the paths to all files that contain Nativewind classes.
   content: ["./app/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
   presets: [require("nativewind/preset")],
   theme: {
-    extend: {},
+    extend: {
+      // 👇 Map Tailwind font utilities to your loaded RN font families
+      fontFamily: {
+        nunito: ["NunitoSans-Regular"],
+        "nunito-bold": ["NunitoSans-Bold"],
+        "nunito-italic": ["NunitoSans-Italic"],
+      },
+    },
   },
   plugins: [],
-}
+};
