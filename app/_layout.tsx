6fe35055efb0d812fffb53c709feeb10863e2593
Waqar Khan <waqar.mrwt424@gmail.com>
2025-09-30 14:13:32 +0500

    Initial commit

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,4 +1,3 @@
-
 import { Stack } from "expo-router";
 import { StatusBar } from "expo-status-bar";
 import "./global.css";
@@ -6,7 +5,7 @@ import "./global.css";
 export default function RootLayout() {
   return (
     <>
-      <StatusBar style="dark" />
+      <StatusBar style="light" backgroundColor="#4F46E5" translucent={false} />
       <Stack screenOptions={{ headerShown: false }}>
         <Stack.Screen name="index" options={{ headerShown: false }} />
       </Stack>
