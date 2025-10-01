17290c1df94af31e24083e0312d49c4d8453b017
Waqar Khan <waqar.mrwt424@gmail.com>
2025-10-01 21:22:45 +0500

    Initial commit

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,10 +1,14 @@
 import { Stack } from "expo-router";
+import { SafeAreaProvider, initialWindowMetrics } from "react-native-safe-area-context";
 import "./global.css";
 
 export default function RootLayout() {
+
   return (
-    <Stack screenOptions={{ headerShown: false }}>
-      <Stack.Screen name="index" options={{ headerShown: false }} />
-    </Stack>
+    <SafeAreaProvider initialMetrics={initialWindowMetrics}>
+      <Stack screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="index" options={{ headerShown: false }} />
+      </Stack>
+    </SafeAreaProvider>
   );
 }
