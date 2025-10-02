5e5e2fb4879d4329a5a3ae7f07c61f590d1a7c2f
Waqar Khan <waqar.mrwt424@gmail.com>
2025-10-02 19:00:29 +0500

    Initial commit

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -6,10 +6,9 @@ import "./global.css";
 export default function RootLayout() {
   return (
     <SafeAreaProvider initialMetrics={initialWindowMetrics}>
-      
-      <StatusBar style="light" translucent backgroundColor="transparent" />
-
-      <Stack screenOptions={{   headerShown: false }}>
+      {/* Opaque indigo status bar (not translucent) */}
+      <StatusBar style="light" translucent={false} backgroundColor="#4F46E5" />
+      <Stack screenOptions={{ headerShown: false }}>
         <Stack.Screen name="index" options={{ headerShown: false }} />
       </Stack>
     </SafeAreaProvider>
